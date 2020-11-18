0bde17ba25e8c137b72afbbf1652bdb99b01926a
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-11-17 16:03:43 -0800

    create and delete

--- to-do-list/src/containers/TodosContainer.js
+++ to-do-list/src/containers/TodosContainer.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import Todos from '../components/ToDos';
+import CreateTodoForm from '../components/CreateTodoForm';
 import TodoModel from '../models/Todo';
 
 class ToDoContainers extends Component {
   state = {
     todos: [],
+  };
+
+  deleteTodo = (todo) => {
+    TodoModel.delete(todo).then((res) => {
+      let todos = this.state.todos.filter((todo) => {
+        return todo._id !== res.data._id;
+      });
+      this.setState({todos});
+    });
   };
 
   componentDidMount() {
@@ -14,21 +24,34 @@ class ToDoContainers extends Component {
 
   fetchData = () => {
     TodoModel.all().then((res) => {
-      console.log(res);
       this.setState ({
         todos: res.data.todos,
       });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+    });
+  };
+
+  createTodo = (todo) => {
+    let newTodo = {
+      body: todo,
+      completed: false,
+    };
+
+    TodoModel.create(newTodo).then((res) => {
+      let todos = this.state.todos;
+      todos.push(res.data);
+      this.setState({ todos: todos });
+    });
   };
 
   render() {
     return (
       <div className="todosComponent">
+        <CreateTodoForm
+          createTodo={this.createTodo} />
         <Todos
-          todos={this.state.todos} />
+          todos={this.state.todos} 
+          deleteTodo={this.deleteTodo}
+          />
       </div>
       );
     };
--- to-do-list/src/models/Todo.js
+++ to-do-list/src/models/Todo.js
@@ -5,9 +5,18 @@ const endPoint = `https://super-crud.herokuapp.com/todos`;
 class TodoModel {
   static all = () => {
     let request = axios.get(endPoint);
-    console.log(request);
+    return request;
+  };
+  static create = (todo) => {
+    let request = axios.post(endPoint, todo);
+    return request;
+  };
+  static delete = (todo) => {
+    let request = axios.delete(`${endPoint}/${todo._id}`);
     return request;
   };
 };
+
+
 
 export default TodoModel;
